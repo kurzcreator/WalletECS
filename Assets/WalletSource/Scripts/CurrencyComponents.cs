940e9eb2f2cf1c5d611eaa3a4263badeab451bbe
Andrei Halubouski <andrei.halubouski@zephyrmobile.com>
2023-08-31 06:04:28 +0400

    Added enum for currency type

--- Assets/WalletSource/Scripts/CurrencyComponents.cs
+++ Assets/WalletSource/Scripts/CurrencyComponents.cs
@@ -2,13 +2,9 @@ using Unity.Entities;
 
 namespace GameCurrency
 {
-    public struct Gold : IComponentData
+    public struct CurrencyComponent : IComponentData
     {
-        public int Amount;
-    }
-
-    public struct Crystals : IComponentData
-    {
+        public int IdCurrency;
         public int Amount;
     }
 }
---
+++ Assets/WalletSource/Scripts/CurrencyType.cs
@@ -0,0 +1,9 @@
+namespace GameCurrency
+{
+    public enum CurrencyType
+    {
+        None = 0,
+        Gold = 1,
+        Crystal = 2,
+    }
+}
