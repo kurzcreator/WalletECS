d42d19d3698ec20a5295894045cb647199624907
Andrei Halubouski <andrei.halubouski@zephyrmobile.com>
2023-08-31 12:30:36 +0400

    Updated widget

--- Assets/WalletSource/Scripts/UI/CurrencyWidget.cs
+++ Assets/WalletSource/Scripts/UI/CurrencyWidget.cs
@@ -3,28 +3,36 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CurrencyWidget : MonoBehaviour
+namespace GameCurrency
 {
-    [SerializeField] private Button incButton;
-    [SerializeField] private Button decButton;
-    [SerializeField] private TextMeshProUGUI currencyCount;
-
-    public event Action<int> OnClickIncrementCurrency;
-
-    private void OnEnable()
+    public class CurrencyWidget : MonoBehaviour
     {
-        incButton.onClick.AddListener(() => OnClickIncrementCurrency?.Invoke(1));
-        decButton.onClick.AddListener(() => OnClickIncrementCurrency?.Invoke(-1));
-    }
+        public CurrencyType CurrencyType;
+        [SerializeField] private Button incButton;
+        [SerializeField] private Button decButton;
+        [SerializeField] private Button resetButton;
+        [SerializeField] private TextMeshProUGUI currencyCount;
 
-    private void OnDisable()
-    {
-        incButton.onClick.RemoveAllListeners();
-        decButton.onClick.RemoveAllListeners();
-    }
+        public event Action<int, CurrencyType> OnClickIncrementCurrencyCount;
+        public event Action<CurrencyType> OnClickResetCurrencyCount;
 
-    public void SetCurrencyCount(int amount)
-    {
-        currencyCount.text = amount.ToString();
+        private void OnEnable()
+        {
+            incButton.onClick.AddListener(() => OnClickIncrementCurrencyCount?.Invoke(1, CurrencyType));
+            decButton.onClick.AddListener(() => OnClickIncrementCurrencyCount?.Invoke(-1, CurrencyType));
+            resetButton.onClick.AddListener(() => OnClickResetCurrencyCount?.Invoke(CurrencyType));
+        }
+
+        private void OnDisable()
+        {
+            incButton.onClick.RemoveAllListeners();
+            decButton.onClick.RemoveAllListeners();
+            resetButton.onClick.RemoveAllListeners();
+        }
+
+        public void SetCurrencyAmount(int amount)
+        {
+            currencyCount.text = amount.ToString();
+        }
     }
 }
