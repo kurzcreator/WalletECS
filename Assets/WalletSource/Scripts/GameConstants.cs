ab315cac9a14362b1f6e146830b63de4be006626
Andrei Halubouski <andrei.halubouski@zephyrmobile.com>
2023-08-31 05:22:56 +0400

    Added key for PlayerPrefs

---
+++ Assets/WalletSource/Scripts/GameConstants.cs
@@ -0,0 +1,9 @@
+
+public static class GameConstants
+{
+	public static class PlayerPrefsKeys
+	{
+		public const string GOLD_KEY = "gold_currency";
+		public const string CRYSTAL_KEY = "crystal_currency";
+	} 
+}
