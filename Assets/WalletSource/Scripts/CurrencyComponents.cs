96f2dc5026200d5b509683266e9e5fc30d999bb1
Andrei Halubouski <andrei.halubouski@zephyrmobile.com>
2023-08-31 12:31:17 +0400

    Refactor

--- Assets/WalletSource/Scripts/CurrencyComponents.cs
+++ Assets/WalletSource/Scripts/CurrencyComponents.cs
@@ -2,9 +2,22 @@ using Unity.Entities;
 
 namespace GameCurrency
 {
-    public struct CurrencyComponent : IComponentData
+    public struct CurrencyId : IComponentData
     {
-        public int IdCurrency;
+        public int Id;
+    }
+
+    public struct CurrencyAmount : IComponentData
+    {
         public int Amount;
     }
+
+    public struct CurrencyChangeEvent : IComponentData
+    {
+        public int Value;
+    }
+
+    public struct CurrencyResetEvent : IComponentData {}
+
+    public struct CurrencyHasChanged : IComponentData {}
 }
--- Assets/WalletSource/Scripts/CurrencyInitSystem.cs
+++ Assets/WalletSource/Scripts/CurrencyInitSystem.cs
@@ -1,40 +1,44 @@
-using GameCurrency;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Transforms;
-using UnityEngine;
 
-[BurstCompile]
-[UpdateInGroup(typeof(InitializationSystemGroup))]
-public partial struct CurrencyInitSystem : ISystem
+namespace GameCurrency
 {
-    private EntityArchetype currencyArchetype;
-
     [BurstCompile]
-    public void OnCreate(ref SystemState state)
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public partial struct WalletInitSystem : ISystem
     {
-        state.RequireForUpdate<UsedCurrencyList>();
-        currencyArchetype = state.EntityManager.CreateArchetype(typeof(CurrencyComponent));
-    }
+        private EntityArchetype currencyArchetype;
 
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state) 
-    {
-        state.Enabled = false;
-
-        var currencyTypes = SystemAPI.GetSingleton<UsedCurrencyList>();
-        ref var currencyList = ref currencyTypes.CurrencyList.Value.Value;
-
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-
-
-        for (int i = 0; i < currencyList.Length; i++)
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
         {
-            var currencyEntity = ecb.CreateEntity(currencyArchetype);
-            ecb.SetComponent(currencyEntity, new CurrencyComponent { IdCurrency = currencyList[i], Amount = 56});
+            state.RequireForUpdate<UsedCurrencyList>();
+            currencyArchetype = state.EntityManager.CreateArchetype(typeof(CurrencyId), typeof(CurrencyAmount));
         }
 
-        ecb.Playback(state.EntityManager);
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+
+            var entity = SystemAPI.GetSingletonEntity<UsedCurrencyList>();
+            var currencyTypes = state.EntityManager.GetComponentData<UsedCurrencyList>(entity);
+            ref var currencyList = ref currencyTypes.CurrencyList.Value.Value;
+
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            var currencyBuffer = ecb.AddBuffer<CurrencyEntityReferenceBufferElement>(entity);
+
+            for (int i = 0; i < currencyList.Length; i++)
+            {
+                var currencyEntity = ecb.CreateEntity(currencyArchetype);
+                ecb.SetComponent(currencyEntity, new CurrencyAmount { Amount = 0 });
+                ecb.SetComponent(currencyEntity, new CurrencyId { Id = currencyList[i] });
+                currencyBuffer.Add(currencyEntity);
+            }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
+        }
     }
 }
